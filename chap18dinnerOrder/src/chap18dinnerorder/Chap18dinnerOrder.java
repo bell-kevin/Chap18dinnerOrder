a24f2fe52e52f894675735d14ca9580d5adac055
Kevin Bell <bellKevin@pm.me>
2022-09-28 12:27:51 -0600

    Update Chap18dinnerOrder.java

--- chap18dinnerOrder/src/chap18dinnerorder/Chap18dinnerOrder.java
+++ chap18dinnerOrder/src/chap18dinnerorder/Chap18dinnerOrder.java
@@ -14,41 +14,43 @@ import java.util.*;
 import javax.swing.border.*;
 
 public class Chap18dinnerOrder extends JFrame {
-JFrame frame = new JFrame("Dinner Order");
+
+    JFrame frame = new JFrame("Dinner Order");
     private static final int WIDTH = 250;
     private static final int HEIGHT = 325;
-    private JComboBox appetizer;
     private String[] appetizerOptions = {"potato skins", "nachos", "buffalo wings"};
-    private JComboBox mainDish;
+    private JComboBox appetizer = new JComboBox(appetizerOptions);
     private String[] mainDishOptions = {"cheeseburger", "pasta", "fish and chips"};
-    private JComboBox dessert;
+    private JComboBox mainDish = new JComboBox(mainDishOptions);
     private String[] dessertOptions = {"brownie", "apple pie", "ice cream"};
+    private JComboBox dessert = new JComboBox(dessertOptions);
     private JButton button = new JButton("Submit");
     private JLabel result;
     JLabel prompt = new JLabel("Welcome to My Diner");
-     
-//******************************************************************************
 
+//******************************************************************************
     public Chap18dinnerOrder() {
         setTitle("Dinner Order");
         setSize(WIDTH, HEIGHT);
-       
-        setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         createContents();
-        setVisible(true);
+        frame.setVisible(true);
     } // end constructor method
 //******************************************************************************
 
     private void createContents() {  // Create components and add them to window 
         Listener listener = new Listener();
         result = new JLabel();
-        add(prompt);
-        add(appetizer);
-        add(mainDish);
-        add(dessert);
-        add(button);
-        add(result);
+        frame.setSize(480, 640);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+        frame.add(prompt);
+        frame.add(appetizer);
+        frame.add(mainDish);
+        frame.add(dessert);
+        frame.add(button);
+        frame.add(result);
+        frame.setVisible(true);
         button.addActionListener(listener);
     } // end createContents method
 //******************************************************************************
